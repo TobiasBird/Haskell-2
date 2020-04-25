d9a9f2dd76a72049a000234b04f5b381f9671eb2
harperSwenson <56015648+harperSwenson@users.noreply.github.com>
2020-04-25 16:01:35 -0700

    Added newlines

--- homework2.hs
+++ homework2.hs
@@ -9,7 +9,7 @@ data GateFn = And
 data Links = From (Int,Int) (Int,Int) Links | EmptyL
 
 --2.b--
---halfAdder = A (B (1,Xor) (B (2,And)) EmptyG) (From (1,1) (2,1) (From (1,2) (2,2) EmptyL))
+halfAdder = A (B (1,Xor) (B (2,And) EmptyG)) (From (1,1) (2,1) (From (1,2) (2,2) EmptyL))
 
 --2.c--
 ppCircuit :: Circuit -> String
@@ -17,7 +17,7 @@ ppCircuit (A g l) = ppGates g ++ ppLinks l
 
 ppGates :: Gates -> String
 ppGates EmptyG = ""
-ppGates (B (int1,str) next) = show int1++":"++ppGateFn str++";"++ppGates next
+ppGates (B (int1,str) next) = show int1++":"++ppGateFn str++";\n"++ppGates next
 
 ppGateFn :: GateFn -> String
 ppGateFn And = "and"
@@ -27,4 +27,4 @@ ppGateFn Not = "not"
 
 ppLinks :: Links -> String
 ppLinks EmptyL = ""
-ppLinks (From (int1,int2) (int3,int4) next) = "from "++show int1++"."++show int2++" to "++show int3++"."++show int4++";"++ppLinks next 	 
+ppLinks (From (int1,int2) (int3,int4) next) = "from "++show int1++"."++show int2++" to "++show int3++"."++show int4++";\n"++ppLinks next 	 
